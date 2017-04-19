e2c3bf51abb1c9327f6eedd5ab3e341fc0fd8837
国金 <guojin@guojindeMac-mini.local>
2017-04-19 16:54:09 +0800

    no message

---
+++ UIColor+UIcolor_RGB.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+UIcolor_RGB.h
+//  GuoJin001
+//
+//  Created by 国金 on 17/4/19.
+//  Copyright © 2017年 seasar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+@interface UIColor (UIcolor_RGB)
++ (UIColor*)colorWithHexString:(NSString*)stringToConvert;
+@end
